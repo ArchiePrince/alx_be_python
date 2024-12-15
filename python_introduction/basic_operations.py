fe1bbbd1cc34e9158f40c0ecd4751e4ff5d0aac8
ArchiePrince <acheampongprince77@gmail.com>
2024-12-15 15:42:17 -0500

    Added basic_operation.py for basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,22 @@
+print("***************************************************")
+print("This solution performs simple arithmetic operations")
+print("***************************************************")
+
+#declaration of variables to be stored
+number1 = 10
+number2 = 5
+
+#addition
+addition = number1 + number2
+
+#subtraction
+subtract = number1 - number2
+
+#multiplication
+multiply = number1 * number2
+
+
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtract}")
+print(f"Multiplication of {number1} and {number2} is {multiply}")
+
