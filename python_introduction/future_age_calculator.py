b17a5ca4e56342389652ec42d71f89843fded8cc
ArchiePrince <acheampongprince77@gmail.com>
2024-12-15 17:04:18 -0500

    Added comments to the program future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -4,14 +4,17 @@ print("*************************************")
 
 
 #Receive user's age input
-
 age = int(input("How old are you? "))
 
+#Assume the current year is 2023 and the expected year of age is 2050
 CURRENT_YEAR = 2023
 EXPECTED_YEAR = 2050
 
+#Computed difference of age in 2050 from current year
 DIFF_YEAR = EXPECTED_YEAR - CURRENT_YEAR
 
+#Calculated result of age in 2050
 result = age + DIFF_YEAR
 
+#Displayed output of results
 print(f"\nIn {EXPECTED_YEAR}, you will be {result} years old.")
