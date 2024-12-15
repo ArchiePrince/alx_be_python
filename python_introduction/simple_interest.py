64a93da248ed315596758e6c55068b3d70fcc35b
ArchiePrince <acheampongprince77@gmail.com>
2024-12-15 15:57:51 -0500

    Modified simple_interest.py  variable assignments

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -5,13 +5,13 @@ print("***************************************************")
 #Definition of identifiers
 
 #Principal amount (initial investment)
-principal: int = 1000
+principal = 1000
 
 #Annual interest rate (as decimal)
-rate: float = 0.05 #representing 5% annual rate
+rate = 0.05 #representing 5% annual rate
 
 #Time the money is invested
-time: int = 3 # representing 3 years
+time = 3 # representing 3 years
 
 #Calculating the simple interest earned on an investment over 3 years
 interest = principal * rate * time
