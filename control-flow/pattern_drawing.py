4b59367e06f35799a0e649d8f758944d935f489a
ArchiePrince <acheampongprince77@gmail.com>
2024-12-22 17:05:11 -0500

    Modified pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,11 +1,13 @@
 # Title: Drawing Patterns with Nested Loops
 # Author: Archie Prince
-# Filename: multiplication_table.py
+# Filename: pattern_drawing.py
 # Date: 22.12.2024
 # Objective: Utilize while loops and nested for loops to draw a simple text-based pattern.
 
+
 print('Draw Patterns with Nested Loops!\n')
 
+#Prompt user for pattern size / length of square
 length = int(input('Enter the size of the pattern: '))
 i = 0
 
